c0339d521fe15301d16e9f4062c8d7a3825e6302
Juan Martin Gimenez <j.martin.utn@gmail.com>
2022-07-16 19:38:56 -0300

    prettier rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {},
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
