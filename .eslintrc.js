36651a1f5eddb9511d7887ffa28f6a04c8a52410
Juan Martin Gimenez <j.martin.utn@gmail.com>
2022-07-16 16:53:12 -0300

    eslint and prettier added

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
