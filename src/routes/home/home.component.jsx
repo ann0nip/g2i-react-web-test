353f8f2c1e92b16b243406198918f25aa926f1f8
Juan Martin Gimenez <j.martin.utn@gmail.com>
2022-07-19 00:12:26 -0300

    base styles and home UI

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import './home.styles.css';
 
 const Home = () => {
   return (
-    <div>
-      <div className="header">Welcome to the Trivia Challenge!</div>
-      <div className="card">
-        <div className="primary_content">
-          You will be presented with 10 True or False questions.
+    <section className="container">
+      <div className="container_box">
+        <header className="header">
+          <span>Welcome to the </span>
+          <h1>
+            <span className="">Trivia</span>
+            <span className=""> Challenge!</span>
+          </h1>
+        </header>
+        <div className="card">
+          <div className="text_content">
+            <p>
+              You will be presented with 10 True or False questions.
+              <br />
+              <br />
+              Can you score 100%?
+            </p>
+          </div>
+          <div className="action_content"></div>
         </div>
-        <div className="secondary_content">Can you score 100%?</div>
+        <div className="button_section">
+          <button className="button-53" role="button">
+            BEGIN
+          </button>
+        </div>
       </div>
-      <div className="button">BEGIN</div>
-    </div>
+    </section>
   );
 };
 
